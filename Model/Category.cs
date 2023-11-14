18330bcaf6c8c108f91bd6eb9b91d979f06af6d5
Christina Kelm <ckelm1@my.wctc.edu>
2023-11-14 14:35:09 -0600

    First Commit

---
+++ Model/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace NWConsole.Model
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Yo! - Enter the name!")]
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
